ba3328619159296893ec382ad89177c3caaf1c03
shei25420 <chegeeric66@gmail.com>
2025-05-28 23:23:59 +0300

    init

---
+++ mqtt_sudh2.cpp
@@ -0,0 +1,12 @@
+﻿// mqtt_sudh2.cpp : Defines the entry point for the application.
+//
+
+#include "mqtt_sudh2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
